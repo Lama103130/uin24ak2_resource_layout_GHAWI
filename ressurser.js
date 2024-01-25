2e92012ffa589beecaaa2d363160f3cc98a898be
Lama103130 <112017575+Lama103130@users.noreply.github.com>
2024-01-25 21:43:40 +0100

    Add files via upload

---
+++ ressurser.js
@@ -0,0 +1,139 @@
+const resources = [
+    {
+        category: "HTML",
+        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "CSS",
+        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "JavaScript",
+        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "React",
+        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Sanity and headless CMS",
+        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
+
+window.onload = function() {
+
+    
+    Content('HTML');
+
+  
+};
+
+
+
+function Content(category) {
+    var titleElement = document.getElementById("categoryTitle");
+    var textElement = document.getElementById("categoryText");
+    var linksList = document.getElementById("links");
+
+    // Finne den valgte kategorien  
+    var selectedCategory = resources.find(item => item.category === category);
+
+    // oppdate inneholdet basert på kategorien
+    if (selectedCategory) {
+        titleElement.textContent = selectedCategory.category;
+        textElement.textContent = selectedCategory.text;
+
+        // Fjerne eksisterende linker basert på kategorien
+        linksList.innerHTML = "";
+
+        // legge til linker til den valgte kategrien
+        selectedCategory.sources.forEach(source => {
+            var listItem = document.createElement("li");
+            var link = document.createElement("a");
+            link.href = source.url;
+            link.textContent = source.title;
+            listItem.appendChild(link);
+            linksList.appendChild(listItem);
+        });
+    } else {
+        titleElement.textContent = 'Unknown Category';
+        textElement.textContent = 'Content not available for this category.';
+    }
+}
+
+
